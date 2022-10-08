36884c36703b470ef3bdecbcf9fdbc9418a70d9d
Error Code <110310532+CodeAEC@users.noreply.github.com>
2022-10-08 18:12:57 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+print("How old are you?: ")
+age = int(input(""))
+
+if age > 18:
+     print("You are old enough to watch horror movie!")
+elif age < 18:
+	print("To young to watch horror movie!")
+elif age == 18:
+	print("You are old enough to watch horror movie!")
+
+print("")
+input("Press any key to continue...")
